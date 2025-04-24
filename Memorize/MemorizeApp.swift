fa0a8de363916438bddbd1359f1fc4134592ddfe
Aleksandr Valter <valtertomsk@gmail.com>
2025-04-24 10:55:05 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Александр Вальтер on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
